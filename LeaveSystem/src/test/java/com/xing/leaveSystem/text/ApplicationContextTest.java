a3e238ba7e0d6ef92d76fc6ab074b332636d7625
liuxingroom <932676523@qq.com>
2017-04-30 21:59:25 +0800

    添加拦截器

--- LeaveSystem/src/test/java/com/xing/leaveSystem/text/ApplicationContextTest.java
+++ LeaveSystem/src/test/java/com/xing/leaveSystem/text/ApplicationContextTest.java
@@ -1,14 +1,14 @@
-package com.xing.leaveSystem.text;
-
-import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class ApplicationContextTest {
-	
-	@Test
-	public void getApplicationContext(){
-		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("spring/applicationContext.xml");
-		System.out.println(applicationContext.getBean("dataSource"));
-	}
-}
+//package com.xing.leaveSystem.text;
+//
+//import org.junit.Test;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+//public class ApplicationContextTest {
+//	
+//	@Test
+//	public void getApplicationContext(){
+//		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("spring/applicationContext.xml");
+//		System.out.println(applicationContext.getBean("dataSource"));
+//	}
+//}
